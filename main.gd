061709d6eef899d304b54f3e97effe4ffa0ade6e
vyacheslav.podolskiy <vyacheslav.podolskiy.dev@gmail.com>
2026-04-23 15:12:02 +0300

    smooth counter has been implemented

--- main.gd
+++ main.gd
@@ -1,7 +1,8 @@
 extends Control
 
-var gold: int = 0
+var gold: float = 0.0
 var gold_boost_cost: int = 10
+var ui_update_timer := 0.0
 var buildings = []
 
 @onready var gold_label = $GoldLabel
@@ -47,13 +48,18 @@ func _on_gold_button_pressed():
 	update_ui()
 
 func update_ui():
-	gold_label.text = "Золото: " + str(gold) + \
+	gold_label.text = "Золото: %.1f" % gold + \
 	"\n(+" + str(get_total_income()) + "/сек)"
 	update_buildings_ui()
 
-func _on_auto_gold_timer_timeout():
-	gold += get_total_income()
-	update_ui()
+func _process(delta):
+	var income_per_second = get_total_income()
+	gold += income_per_second * delta
+
+	ui_update_timer += delta
+	if ui_update_timer >= 0.05: # 20 раз в секунду
+		update_ui()
+		ui_update_timer = 0.0
 
 func buy_building(index):
 	var b = buildings[index]
